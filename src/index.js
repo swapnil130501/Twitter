e257e74bae2aa3532b6a20197e78170eb2685978
swapnil <swapnil130501@gmail.com>
2023-06-12 20:50:19 +0530

    added hooks

--- src/index.js
+++ src/index.js
@@ -11,16 +11,11 @@ app.listen(PORT, async () => {
     console.log(`Server started at port ${PORT}`);
     await connect();
     console.log('Mongo db connected');
-    // const tweet = await Tweet.create({
-    //     content: 'Second tweet',
-    //     userEmail: 'test@test.com'
-    // });
-    // const tweets = await Tweet.find({ userEmail: 'test@test.com' });
-    // const tweet = await Tweet.findById('64849567a0de1e5e29431a09');
-    // tweet.userEmail = 'test1@test.com';
-    // await tweet.save();
-    // console.log(tweet);
+
     const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.getAll(0,4);
-    console.log(tweet[0].contentWithEmail);
+    const tweet = await tweetRepo.create({
+        content: 'with hooks now',
+        userEmail: 'test@test.com' 
+    });
+    console.log(tweet);
 });
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -23,5 +23,10 @@ tweetSchema.virtual('contentWithEmail').get(function process(){
     return this.content + "\n" + "Created by: " + this.userEmail;
 });
 
+tweetSchema.pre('save', function(next){
+    console.log('Inside a hook');
+    this.content = this.content + '....';
+    next();
+});
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
