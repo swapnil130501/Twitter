f9c6790d5761011d497cacd33dbc99a787ae15a4
swapnil <swapnil130501@gmail.com>
2023-06-12 23:34:27 +0530

    added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -11,11 +11,4 @@ app.listen(PORT, async () => {
     console.log(`Server started at port ${PORT}`);
     await connect();
     console.log('Mongo db connected');
-
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.create({
-        content: 'with hooks now',
-        userEmail: 'test@test.com' 
-    });
-    console.log(tweet);
 });
