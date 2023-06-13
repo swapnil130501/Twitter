43aeb3ad1e3ff23246515be411c6f8d3eb4cbe59
swapnil <swapnil130501@gmail.com>
2023-06-13 21:21:13 +0530

    added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const app = express();
 const connect = require('./config/database');
-
+const app = express();
 const PORT = 3000;
-
-const Tweet = require('./models/tweet');
-
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
 
 app.listen(PORT, async () => {
     console.log(`Server started at port ${PORT}`);
     await connect();
     console.log('Mongo db connected');
-
-    const tweets = await Tweet.find({
-        content: ["test", "Second tweet", "abc"]
-    });
-    console.log(tweets);
 });
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,16 +1,28 @@
-const {TweetRepository} = require('../repository/index');
+const { TweetRepository, HashtagRepository } = require('../repository/index');
 
 class TweetService{
     constructor(){
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
     async create(data){
         const content = data.content;
         const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1)); // this regex extracts hashtags
-        console.log(tags);
         const tweet = await this.tweetRepository.create(data);
-        return tweet;  
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        console.log(alreadyPresentTags);
+        let titleOfPresentTags = alreadyPresentTags.map(tags => tags.title);
+        let newTags = tags.filter(tag => !titleOfPresentTags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets: [tweet.id]}
+        });
+        const response = await this.hashtagRepository.bulkCreate(newTags);
+        alreadyPresentTags.forEach((tag) => {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        });
+        return tweet;
     }
 }
 
